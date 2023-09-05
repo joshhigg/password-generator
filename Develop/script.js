9791861c98dcbbf1609515bfb2766f2f2b30befe
joshhigg <136753578+joshhigg@users.noreply.github.com>
2023-09-05 14:18:14 -0600

    prompts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = window.prompt("How long would you like your password to be?\n\nBetween 8 and 128 characters");
 
+  if(!passwordLength) {
+    return;
+  } else if(passwordLength < 8) {
+    window.alert("Error, please enter a number between 8 and 128");
+    return;
+  } else if(passwordLength > 128) {
+    window.alert("Error, please enter a number between 8 and 128");
+    return;
+  }
+
+  var includeLowercase = window.confirm("Should it include lowercase letters?");
+  var includeUppercase = window.confirm("Should it include uppercase letters?");
+  var includeSpecial = window.confirm("Should it include special characters?");
+}
 
 // Write password to the #password input
 function writePassword() {
